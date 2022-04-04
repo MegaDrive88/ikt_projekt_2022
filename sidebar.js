45ccc6dff5a95b81429da6f155247d2393a70777
MegaDrive88 <podlipnik.adam@students.jedlik.eu>
2022-04-04 13:56:53 +0200

    uwuuu

---
+++ sidebar.js
@@ -0,0 +1,8 @@
+// function openNav() {
+//     document.getElementById("#menu").style.width = "20%";
+//     document.getElementById("#main").style.marginLeft = "20%";
+//     document.getElementById("");
+// function closeNav() {
+//     document.getElementById("#menu").style.width = "0";
+//     document.getElementById("#main").style.marginLeft= "40%";
+//majd megoldom
