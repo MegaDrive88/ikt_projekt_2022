5b2a93ba1a95107bb0590bac669b8ca98e680128
mzalan <marquetant.zalan@students.jedlik.eu>
2022-04-19 14:30:48 +0200

    egyelőre ennyi

--- keresscript.js
+++ keresscript.js
@@ -1,19 +1,19 @@
 var elem = document.documentElement;
+var a = 50;
 var x = screen.width;
 var y = screen.height;
 var runTime = 1;
-var i = 0;
-var caller;
-const tiles = new Array(400);  
+const tiles = new Array(a*a);  
 if(x >= y)  {
-    tilesize = 0.9*y/20;
+    tilesize = 0.9*y/a;
     pushY = 0.05*y;
-    pushX = (x-(tilesize*20))/2;
+    pushX = (x-(tilesize*a))/2;
+
 }
 else {
-    tilesize = x/20;
+    tilesize = x/a;
     pushX = 0;
-    pushY = (y-(tilesize*20))/2;
+    pushY = (y-(tilesize*a))/2;
 }
 
 function openFullscreen() {
@@ -29,12 +29,13 @@ function openFullscreen() {
 }
 
 function Editor() {
+    document.querySelector("#s1").className="s1";
     document.querySelector("#main").className="no";
     document.querySelector("#menu").className="no";
     document.querySelector("#collapser").className="no";
     document.querySelector("#collapser2").className="no";
 
-for(i=0; i<400; i++) {
+for(i=0; i<a*a; i++) {
     tiles[i] = document.createElement("span");
     tiles[i].className = "canvastile";
     tiles[i].style.width=tilesize+"px";
@@ -42,7 +43,7 @@ for(i=0; i<400; i++) {
     tiles[i].style.top=pushY+"px";
     tiles[i].style.left=pushX+"px";
     document.querySelector("#canvas").appendChild(tiles[i]);
-    if(runTime <= 19) {
+    if(runTime <= a-1) {
         runTime++;
         pushX += tilesize;
     }
@@ -50,22 +51,18 @@ for(i=0; i<400; i++) {
         runTime = 1;
         pushY += tilesize;
         if(x>=y) {
-            pushX = (x-(tilesize*20))/2;
+            pushX = (x-(tilesize*a))/2;
         }
         else {
             pushX=0;
         }
-    }
-    
+    } 
 }
-for(i = 0; i<400; i++) {
-    tiles[i].addEventListener("click", coloring )
-}
-    
-}
+for(i = 0; i<a*a; i++) {
+    tiles[i].addEventListener("mousedown", coloring);
+}  
 
-
-function coloring(event) {
-    
-    event.target.style.width=10*tilesize+"px";
+}
+function coloring(event) { 
+    event.target.style.backgroundColor="red";
 }
