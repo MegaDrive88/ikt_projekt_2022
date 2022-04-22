a27941adeabc0e3b821866f73363a59a938b29eb
mzalan <marquetant.zalan@students.jedlik.eu>
2022-04-22 21:06:14 +0200

    színek slotjai elrendezve de még nem tökéletesen reszponzív

--- keresscript.js
+++ keresscript.js
@@ -1,12 +1,14 @@
 var elem = document.documentElement;
 var a = 50;
-x = screen.width;
-y = screen.height;
-var coloruileft = x/4;
-var coloruitop = y/5;  
+var x = screen.width;
+var y = screen.height; 
 var runTime = 1;
+var slotRun = 1;
+var columns = 0;
 var color = "black";
+var uitemp = document.querySelector("#uitemp");
 const tiles = new Array(a*a);  
+
 if(x >= y)  {
     tilesize = 0.9*y/a;
     pushY = 0.05*y;
@@ -18,6 +20,12 @@ else {
     pushX = 0;
     pushY = (y-(tilesize*a))/2;
 }
+const slots = new Array(48);
+var slotsize = y/12-y/108;
+var slotY = slotsize/9;
+var slotX = pushX/2+(0.66*(pushX/4-slotsize));
+var yAdd = slotsize + slotsize/9;
+
 function openFullscreen() {
     if(elem.requestFullscreen) {
     elem.requestFullscreen();
@@ -30,12 +38,45 @@ function openFullscreen() {
     }
 }
 function Editor() {
-    editor = true;
-    document.querySelector("#s1").className="s1";
     document.querySelector("#main").className="no";
     document.querySelector("#menu").className="no";
     document.querySelector("#collapser").className="no";
     document.querySelector("#collapser2").className="no";
+    
+    for(i = 0; i < 48; i++) {
+        slots[i] = document.createElement("span");
+        slots[i].className = "slot";
+        slots[i].style.width= slotsize+"px";
+        slots[i].style.height = slotsize +"px";
+        slots[i].style.top=slotY+"px";
+        slots[i].style.left=slotX+"px";
+        document.querySelector("#uitemp").appendChild(slots[i]);
+        if(slotRun <= 11) {
+            slotY += yAdd;
+            slotRun++;
+        }
+        else {
+            columns++;
+            slotY = slotsize/9;
+            if(columns==2) {
+                slotX += a*tilesize+2*0.66*(pushX/4-slotsize) + slotsize;
+            }
+            else {
+                slotX += 0.66*(pushX/4-slotsize)+ slotsize;
+            }
+            
+            slotRun = 1;
+            
+        }
+        
+    }
+    
+
+    uitemp.className = "uitemp";
+    uitemp.style.top="0px";
+    uitemp.style.height = "100%";
+    uitemp.style.left = pushX/2 + "px";
+    uitemp.style.width = a*tilesize+pushX+ "px";
 
 for(i=0; i<a*a; i++) {
     tiles[i] = document.createElement("span");
@@ -44,7 +85,7 @@ for(i=0; i<a*a; i++) {
     tiles[i].style.height =tilesize+"px";
     tiles[i].style.top=pushY+"px";
     tiles[i].style.left=pushX+"px";
-    document.querySelector("#canvas").appendChild(tiles[i]);
+    document.querySelector("#ui").appendChild(tiles[i]);
     if(runTime <= a-1) {
         runTime++;
         pushX += tilesize;
