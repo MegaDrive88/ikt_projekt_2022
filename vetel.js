37ce6a7661c8c217499756c612fdf587943413e2
MegaDrive88 <podlipnik.adam@students.jedlik.eu>
2022-04-22 17:26:40 +0200

    😭😭😭😭😭

--- vetel.js
+++ vetel.js
@@ -1,1 +1,8 @@
-//AAAAAAAAAAAAAAAAAAAAAAAaaAAAAAaAAaaAaaaaaaAaaaAaaaAAAAaaaaaaaaaaAAAAaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+window.onload = kepvalasztas() ;
+function kepvalasztas(owo){
+    var kep = document.getElementById('1');
+    document.getElementById('egesz').innerHTML = kep;
+}
+//'pictures/nfts/nft' + kep + '.png'
+//    document.getElementById('nftitself').src = kep;
+//f@szom kivan
