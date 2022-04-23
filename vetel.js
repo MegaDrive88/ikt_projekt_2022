9870f219ef79d37005e73147f810cf14ccabb11b
MegaDrive88 <podlipnik.adam@students.jedlik.eu>
2022-04-23 11:55:58 +0200

    sztem a vetel.html már nem is kell

--- vetel.js
+++ vetel.js
@@ -1,7 +1,21 @@
-window.onload = kepvalasztas() ;
-function kepvalasztas(){
-    var kep = document.getElementById('15').id;
+window.onload = kepvalasztas(15, 2, 3) ;
+function kepvalasztas(kep, nev, ar){
     document.getElementById('nftitself').src = 'pictures/nfts/nft' + kep + '.png';
+    document.getElementById('nftar').innerHTML = ar + 'Ξ';
+    document.getElementById('nftnev').innerHTML = nev;
+    document.getElementById('nftitself').style.width = "30rem";
+    document.getElementById('nftitself').style.height = "30rem";
+    document.getElementById('valasztott').style.width = "96%";
+    document.getElementById('valasztott').style.height = "91%";
+    document.getElementById('valasztott').style.backgroundColor = "#000000";
+    document.getElementById('nftvetel').innerHTML = "Vásárlás";
+    document.getElementById('nftvetel').style.width = "10rem";
+    document.getElementById('nftvetel').style.height = "2rem";
+    document.getElementById('bezaras').innerHTML = "X";
+    document.getElementById('bezaras').style.width = "2rem";
+    document.getElementById('bezaras').style.height = "2rem";
+}
+function bezaraas(){
+
 }
 //tarolas
-//f@szom kivan
