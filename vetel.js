a7bd311d17e35f46f0401ef1b6d24d6e87c1e620
MegaDrive88 <podlipnik.adam@students.jedlik.eu>
2022-04-24 14:48:49 +0200

    Úristen műkszik az áruház

--- vetel.js
+++ vetel.js
@@ -16,8 +16,7 @@ function kepvalasztas(kep, nev, ar, akcio){
     document.getElementById('bezaras').style.height = "2rem";
     document.getElementById('nftnev').style.width = "max-content";
     document.getElementById('nftnev').style.height = "min-content";
-    var uwu = kep;
-    localStorage.setItem('kepszamm') = uwu;
+    kep = localStorage.setItem('szama', kep)
 }
 function bezaraas(){
     document.getElementById('valasztott').innerHTML = '<img src=""  id="nftitself" style="height: 0; width: 0; margin-left: 2rem; margin-top: 2rem;"><div style="float: right; margin-top: 2rem; margin-right: 50%;"><h1 style="height: 0; width: 0; margin-left: 0rem; margin-bottom: 2.5rem;" id="nftnev"></h1><br><div class="row"><h3 style="height: 0; width: 0; margin-left: 0rem; margin-bottom: 0rem;" id="nftar"></h3><h3 style="color: red; margin-left: 3.3rem; margin-bottom: 0; width: 0;" id="akcioe"></h3></div><br><button type="button" onclick="vasarlas()" style="height: 0; width: 0; padding: 0 0 0 0; color: white; margin-left: 0rem;" id="nftvetel"></button> <br><button type="button" onclick="bezaraas()" style="height: 0; width: 0; padding: 0 0 0 0; color: white; margin-left: 2rem; background-color: darkred; position: fixed; top: 12px !important; right: 32px !important;" id="bezaras"></button></div>';
@@ -25,7 +24,7 @@ function bezaraas(){
     document.getElementById('valasztott').style.height = "0";
 }
 function vasarlas(){
-    var kepszam = localStorage.getItem('kepszamm');
+    var kepszam = localStorage.getItem('szama', 'szama')
     document.getElementById('lelink').href = 'pictures/nfts/nft' + kepszam + '.png';
     alert("Az NFT-d megvásárlásra került");
     bezaraas();
